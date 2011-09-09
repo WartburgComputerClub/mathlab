ef1e0ae462e24c4def7612d6ba9ea200d68afc59
Andrew Reisner <andrew.reisner@gmail.com>
2011-09-09 03:11:39 -0700

    another bugfix

--- js/user.js
+++ js/user.js
@@ -43,7 +43,7 @@ function update()
     take = $('#taken').val();
     fute = $('#future').val();
     studentid = $('#studentid').val();
-    $('#formspace').html('<img src="images/spinner.gif"> &nbsp; Processing...').load('modifystudent.php',{id: studentid,firstname: first,lastname: last,interest: inter, taken: take,future: fute},function{$('#flex').flexReload();});
+    $('#formspace').html('<img src="images/spinner.gif"> &nbsp; Processing...').load('modifystudent.php',{id: studentid,firstname: first,lastname: last,interest: inter, taken: take,future: fute},function(){$('#flex').flexReload();});
 
 }
 $(document).ready(function(){
