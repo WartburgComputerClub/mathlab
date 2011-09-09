93d41252a1257da186fea764caec510f29ef185c
Andrew Reisner <andrew.reisner@gmail.com>
2011-09-09 05:53:20 -0700

    more session stuff

--- getsessions.php
+++ getsessions.php
@@ -4,7 +4,19 @@ require_once('connect.php');
 
 $db = db_connect();
 $stmt = $db->stmt_init();
-
+if ($stmt->prepare("SELECT distinct DATE_FORMAT(id,'%d/%m/%Y') FROM session"))
+{
+    $stmt->execute();
+    $stmt->bind_result($date);
+    while($stmt->fetch())
+    {
+	echo("<a href='signin.php?date=$date'>");
+	echo($date);
+	echo("</a>");
+	echo('<br />');
+    }
+    $stmt->close();
+}
 
 $db->close();
 ?>
