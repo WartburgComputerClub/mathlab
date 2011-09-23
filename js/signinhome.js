50bedc12ae375bcbe0ab026b78091bbbbf42d9f3
Andrew Reisner <andrew.reisner@gmail.com>
2011-09-23 03:16:37 -0700

    bugfix

--- js/signinhome.js
+++ js/signinhome.js
@@ -1,11 +1,11 @@
 function addSession()
 {
-    var last = new Date($('#last').val());
-    var test = new Date($('#today').val());
-    if (test > last)
-	window.location="signin.php?date=" + $('#today').val();
-    else
-	$('#response').html("<font color='red'>A Session already exists for this date!</font>");
+//    var last = new Date($('#last').val());
+//    var test = new Date($('#today').val());
+//    if (test > last)
+    window.location="signin.php?date=" + $('#today').val();
+//    else
+//	$('#response').html("<font color='red'>A Session already exists for this date!</font>");
 }
 $(document).ready(function(){
     $('#sessions').html('<img src="images/spinner.gif"> &nbsp; Processing...').load('getsessions.php');
