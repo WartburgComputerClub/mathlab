3ce44753f6b3fb12e07c0e055fa1658639786652
Andrew Reisner <andrew.reisner@gmail.com>
2011-10-12 03:38:51 -0700

    generates excel spreadsheet

--- genexcel.php
+++ genexcel.php
@@ -2,41 +2,57 @@
 ob_start();
 include 'Classes/PHPExcel.php';
 require_once('checkuser.php');
-require_once('connect.php');
+require_once('config.php');
+mysql_connect(DBAuth::$host,DBAuth::$user,DBAuth::$password);
+mysql_select_db(DBAuth::$db);
+$user = $_SESSION['user'];
 
 $objReader = PHPExcel_IOFactory::createReader('Excel5');   
 $excel = $objReader->load('sheet.xls');
-$db = db_connect();
-$stmt = $db->stmt_init();
+// Get first exam date
+$sql = "SELECT `exam` FROM `course` WHERE `id`=$user";
+$result = mysql_query($sql);
+$row = mysql_fetch_row($result);
+$exam = $row[0];
+
 
 $excel->setActiveSheetIndex(0);
 $worksheet = $excel->getActiveSheet();
-$sql = 'SELECT lastname,firstname,interest,taken,future FROM student WHERE course=?';
-
-if ($stmt->prepare($sql))
+$sql = "SELECT id,lastname,firstname,interest,taken,future FROM student WHERE course=$user";
+$sql1 = "SELECT * FROM session WHERE student=? AND date < ?";
+$sql2 = "SELECT * FROM session WHERE student=?";
+$result = mysql_query("SELECT id,lastname,firstname,interest,taken,future FROM student WHERE course=$user");
+$row =8;
+while ($row1 = mysql_fetch_row($result))
 {
-    $stmt->bind_param('i',$_SESSION['user']);
-    $stmt->execute();
-    $stmt->bind_result($last,$first,$interest,$taken,$future);
-    $row = 8;
-    while($stmt->fetch())
-    {
-	if ($taken == 1)
-	    $taken = 'Yes';
-	else
-	    $taken = 'No';
+    $id = $row1[0];
+    $last = $row1[1];
+    $first = $row1[2];
+    $interest = $row1[3];
+    $taken = $row1[4];
+    $future = $row1[5];
+    
+    if ($taken == 1)
+	$taken = 'Yes';
+    else
+	$taken = 'No';
+
+    
+    $worksheet->setCellValueByColumnAndRow(6,$row,$last);
+    $worksheet->setCellValueByColumnAndRow(7,$row,$first);
+    $worksheet->setCellValueByColumnAndRow(8,$row,$interest);
+    $worksheet->setCellValueByColumnAndRow(9,$row,$taken);
+    $worksheet->setCellValueByColumnAndRow(10,$row,$future);
+    
+    $sql = "SELECT * FROM session WHERE student=$id AND id < '$exam'";
+    $result1 = mysql_query($sql);
 		
-	$worksheet->setCellValueByColumnAndRow(6,$row,$last);
-	$worksheet->setCellValueByColumnAndRow(7,$row,$first);
-	$worksheet->setCellValueByColumnAndRow(8,$row,$interest);
-	$worksheet->setCellValueByColumnAndRow(9,$row,$taken);
-	$worksheet->setCellValueByColumnAndRow(10,$row,$future);
-    }
+    $worksheet->setCellValueByColumnAndRow(11,$row,mysql_num_rows($result1));	
+    $result1 = mysql_query("SELECT * FROM session WHERE student=$id");
+    $worksheet->setCellValueByColumnAndRow(13,$row,mysql_num_rows($result1));
+    $row++;
 }
 
-//$worksheet->setCellValueByColumnAndRow(6,8,"Hello World!");
-$db->close();
-//$objWriter = PHPExcel_IOFactory::createWriter($excel,'Excel5');
 $objWriter = PHPExcel_IOFactory::createWriter($excel, 'Excel5');
 header('Content-Type: application/vnd.ms-excel');
 header('Content-Disposition: attachment;filename="report.xls"');
