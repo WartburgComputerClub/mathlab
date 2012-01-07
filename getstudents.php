b6a5142f057e069ca9d4d67784618d2b6dc8ec2b
Andrew Reisner <andrew.reisner@gmail.com>
2012-01-06 21:32:21 -0600

    let admin view and edit sessions

--- getstudents.php
+++ getstudents.php
@@ -5,11 +5,19 @@ require_once('connect.php');
 $date = $_POST['date'];
 $db = db_connect();
 $stmt = $db->stmt_init();
-$sql = "SELECT student.id,firstname,lastname FROM student INNER JOIN session on (student.id=session.student) WHERE session.id=? AND student.course=?";
+if ($_SESSION['user'] == 'admin'){
+    $sql = "SELECT student.id,firstname,lastname FROM student INNER JOIN session on (student.id=session.student) WHERE session.id=?";
+}else{
+    $sql = "SELECT student.id,firstname,lastname FROM student INNER JOIN session on (student.id=session.student) WHERE session.id=? AND student.course=?";
+}
 
 if ($stmt->prepare($sql))
 {
-    $stmt->bind_param('si',$date,$_SESSION['user']);
+    if ($_SESSION['user'] == 'admin'){
+	$stmt->bind_param('s',$date);
+    }else{
+	$stmt->bind_param('si',$date,$_SESSION['user']);
+    }
     $stmt->execute();
     $stmt->bind_result($id,$first,$last);
     while($stmt->fetch())
